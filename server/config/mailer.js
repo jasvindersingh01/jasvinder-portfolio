6a367435709f0f3383f25c6bfd86b1ecd49c8302
unknown <jasvindersingh0901@gmail.com>
2026-06-05 17:05:58 +0530

    Fix bug

--- server/config/mailer.js
+++ server/config/mailer.js
@@ -7,7 +7,11 @@ console.log(process.env.EMAIL_USER);
 console.log(process.env.EMAIL_PASS);
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  family: 4,
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
